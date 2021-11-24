0506ea3ca6814b98c2551b9552209f73d754ed61
Olal James <jkeneth.jk@gmail.com>
2021-11-24 05:51:39 +0300

    update on the task2.sh solution

--- task2.sh
+++ task2.sh
@@ -1,14 +1,24 @@
 !/bin/bash
-#create and assign _user variable to get current user. The -n option displays the name
-_user="$(id -u -n)"
+#create and assign currentUser variable to get current user. The -n option displa>
+currentUser="$(id -u -n)"
 #print current user
-echo "Current User : $_user"
+echo "Current User : $currentUser"
 
-#use the _user variable to establish if the current user is root or not
-if [ $_user != 'root' ]; then
+FILE=/tmp/task2.txt
+machinename=$HOSTNAME #declare variable machinename  and assign it $HOSTNAME
+
+#use the currentUser variable to establish if the current user is root or not
+if [ $currentUser != 'root' ]; then
     echo "You must be root to do this."
 
 #check to see if _user equals root. 
-elif [ $_user == 'root' ]; then
+elif [ "$currentUSer" == 'root' ]; then
 echo "I am root"
+
+elif [ -f $FILE ]; then
+  echo "File $FILE does not exist."
+else
+  touch $FILE #create file  
+echo $machinename>$FILE  
 fi #close the if statement
+
